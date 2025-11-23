566c05b5ce5cfb7e6f8d9db47a94cd290cf3a16e
Jayantimishra <mishrajayanti956@gmail.com>
2025-11-23 15:11:32 +0530

    commited

--- Express.js/day-1/index.js
+++ Express.js/day-1/index.js
@@ -36,11 +36,75 @@ app.get('/api/v1/users/:id', (req, res) => {
 app.post('/api/v1/users', (req, res) => {
     const {name, displayname} = req.body;
 
+    const newUser = {
+        id: Userdata.length + 1,
+        name,
+        displayname
+    };
+
+    Userdata.push(newUser);
+
     
-    res.status(201);
-    res.send('Post Request from Express.js');
+    res.status(201).send(
+        {message: 'User created successfully',
+             data: newUser}
+    );
 });
 
+
+// *3. Put Request (it is used to update data on the server)
+app.put('/api/v1/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const {name, displayname} = req.body;
+    const userIndex = Userdata.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+        Userdata[userIndex] = { 
+            id: id,
+            name: name,
+            displayname: displayname
+        };
+        res.status(200).send(
+            {message: 'User updated successfully',
+             data: Userdata[userIndex]}
+        );
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }
+});
+
+// *4. Patch Request (it is used to partially update data on the server)
+app.patch('/api/v1/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const {name, displayname} = req.body;
+    const user = Userdata.find((user) => user.id === id);
+    if (user) {
+        if (name) user.name = name;
+        if (displayname) user.displayname = displayname;
+        res.status(200).send(
+            {message: 'User partially updated successfully',
+             data: user}
+        );
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }   
+
+});
+
+// *5. Delete Request (it is used to delete data from the server)
+app.delete('/api/v1/users/:id', (req, res) => {
+    const id = parseInt(req.params.id); 
+    const userIndex = Userdata.findIndex((user) => user.id === id);
+    if (userIndex !== -1) {
+        const deletedUser = Userdata.splice(userIndex, 1);
+        res.status(200).send(
+            {message: 'User deleted successfully',
+             data: deletedUser}
+        );
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }   
+
+});
 
 
 app.listen(PORT, (req, res) => {
