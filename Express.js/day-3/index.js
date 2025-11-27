87f3130cfad519bf94907cb721089610634f0f2d
Jayantimishra <mishrajayanti956@gmail.com>
2025-11-27 14:36:48 +0530

    commited

--- Express.js/day-3/index.js
+++ Express.js/day-3/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
@@ -8,6 +11,15 @@ app.get('/', (req, res) => {
   res.cookie("name", "JM")
   res.send('Hello, World!');
 });
+
+app.get('/show-cookie', (req, res) => {
+  console.log('Cookies: ', req.cookies);
+  res.send({
+    id: 1,
+    name: 'JM',
+    price: 3000
+  });
+})
 
 app.listen(3050, () => {
   console.log(`Server is running on http://localhost:3050`);
